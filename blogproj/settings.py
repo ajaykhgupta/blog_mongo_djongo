c312be7182350b3fd7584bd4bbe2b0474c122631
ajaykhgupta <ajaykhgupta@gmail.com>
2022-03-06 16:21:50 +0530

    cors configured in settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -52,12 +53,18 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000'
+)
 
 ROOT_URLCONF = 'blogproj.urls'
 
